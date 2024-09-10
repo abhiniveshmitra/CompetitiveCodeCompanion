cf54a7e9e4bf495b942f00d40101da9bba6a9714
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2024-09-10 15:09:10 +0530

    Create factors.cpp

---
+++ factors.cpp
@@ -0,0 +1,22 @@
+vector<lli> factors(lli n)
+{
+    vector<lli> facs;
+    for(lli i=2;i*i<=n;i++)
+    {
+        if(!(n%i))
+        {
+            lli ct=0;
+            while(n%i==0)
+            {
+                n/=i;
+                ct++;
+            }
+            facs.pb(ct);
+        }
+    }
+    if(n>1)
+    {
+        facs.pb(n);
+    }
+    return facs;
+}
