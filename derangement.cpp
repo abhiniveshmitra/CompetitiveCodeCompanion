c5660f39c007e82c12b9668edfa595c05b314514
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2024-09-16 11:33:02 +0530

    Update derangement.cpp

--- derangement.cpp
+++ derangement.cpp
@@ -1,8 +1,11 @@
-lli subfact(lli n)
-{
-    if (n == 0)
-        return 1;
-    if (n == 1)
-        return 0;
-    return n * subfact(n - 1) + power(-1,n);
+lli subfact(lli n) {
+    if (n == 0) return 1;
+    if (n == 1) return 0;
+    lli derangement = 0, prev = 1, curr = 0;
+    for (lli i = 2; i <= n; ++i) {
+        derangement = (i - 1) * (curr + prev) % modu;
+        prev = curr;
+        curr = derangement;
+    }
+    return curr;
 }
