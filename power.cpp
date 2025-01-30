33a62fdb8827b4c20e2805dbe0463a738b4540f3
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-01-30 09:34:44 +0530

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -1,8 +1,8 @@
-lli mul(lli a, lli b) {
+ll mul(ll a, ll b) {
   return  ( a * b % modu);
 }
-lli power(lli a, lli b) {
-  lli res = 1;
+ll power(ll a, ll b) {
+  ll res = 1;
   while (b > 0) {
     if (b & 1) {
       res = mul(res, a);
